e7000dd8bc985054997e14907a13cdc4d0e0d26e
Milan992 <mitic.gsm@gmail.com>
2020-09-01 13:01:34 +0200

    Imported Entity Framework.

--- Nedeljni_5_Milan_Mitic/WpfBetweenUs/Service.cs
+++ Nedeljni_5_Milan_Mitic/WpfBetweenUs/Service.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfBetweenUs.Model;
 
 namespace WpfBetweenUs
 {
@@ -10,3 +11,4 @@ namespace WpfBetweenUs
     {
     }
 }
+
