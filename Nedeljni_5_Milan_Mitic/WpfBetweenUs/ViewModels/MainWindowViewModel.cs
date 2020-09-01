32bf89051238569c1c874271a32edd11d10f4546
Milan992 <mitic.gsm@gmail.com>
2020-09-01 11:42:14 +0200

    Created MainWindowVIewModel class.

---
+++ Nedeljni_5_Milan_Mitic/WpfBetweenUs/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using WpfBetweenUs;
+using WpfBetweenUs.ViewModels;
+
+namespace WpfCompany.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        Service service = new Service();
+        MainWindow main;
+
+        #region Constructors
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string userName;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand logIn;
+
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => LogInExecute(), param => CanLogInExecute());
+                }
+
+                return logIn;
+            }
+        }
+
+        private void LogInExecute()
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanLogInExecute()
+        {
+            return true;
+        }
+
+        private ICommand register;
+
+        public ICommand Register
+        {
+            get
+            {
+                if (register == null)
+                {
+                    register = new RelayCommand(param => RegisterExecute(), param => CanRegisterExecute());
+                }
+
+                return register;
+            }
+        }
+
+        private void RegisterExecute()
+        {
+            try
+            {
+                //Register register = new Register();
+                //register.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanRegisterExecute()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
