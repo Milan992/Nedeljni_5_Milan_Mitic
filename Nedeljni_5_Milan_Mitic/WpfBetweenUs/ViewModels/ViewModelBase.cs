39bf28ad67841a6cc3da7cd948cfc3cd14a49d5c
Milan992 <mitic.gsm@gmail.com>
2020-09-01 11:38:33 +0200

    Created RelayCommand and ViewModelBase classes.

--- Nedeljni_5_Milan_Mitic/WpfBetweenUs/ViewModels/ViewModelBase.cs
+++ Nedeljni_5_Milan_Mitic/WpfBetweenUs/ViewModels/ViewModelBase.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace WpfBetweenUs.ViewModels
 {
-    class ViewModelBase
+    class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
